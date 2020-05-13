82d073213bc798951911d02130035c7e723f18d8
BL <benoit.lena@protonmail.com>
2020-05-13 16:34:32 +0200

    ...

--- helpers/one/classForm.php
+++ helpers/one/classForm.php
@@ -2,15 +2,15 @@
 
     class Form{
         private $data;
-        public $surround = 's';
+        public $search = 's';
 
             public function __construct($data = array()){
                 $this->data = $data;
 
             }
 
-            private function surround($html){
-                return "<{$this->surround}>$html</{$this->surround}>";
+            private function search($html){
+                return "<{$this->search}>$html</{$this->search}>";
             }
 
             private function getValue($index){
@@ -18,31 +18,31 @@
             }
 
             public function input($name){
-                return $this->surround(
+                return $this->search(
                     '<input type="text" name="' . $name . '" value="' . $this->getValue($name) . '">');  
             }
 
-            public function select($pays){
-                return $this->surround(
-                    '<select id="country"  pays="' . $country . '" value="' . $this->getValue($country) . '"><option value="Belgique">Belgium</option></select>');  
+            public function select($country){
+                return $this->search(
+                    '<select id="country"  country="' . $country . '" value="' . $this->getValue($country) . '"><option value="Belgique">Belgium</option></select>');  
             }
 
             public function submit(){
-                return $this->surround('<button type="submit">Envoyer</button>');
+                return $this->search('<button type="submit">Envoyer</button>');
             }
 
             public function textarea($message){
-                return $this->surround(
+                return $this->search(
                     '<textarea  message="' . $message . '" value="' . $this->getValue($message) . '"></textarea>');  
             }
 
             public function radio($radio){
-                            return $this->surround(
+                            return $this->search(
                                 '<input type="radio" name="' . $radio . '" value="' . $this->getValue($radio) . '"><label for="female">Female</label><br>');  
             }
 
             public function check($checkbox){
-                return $this->surround(
+                return $this->search(
                     '<input type="checkbox" name="' . $checkbox . '" value="' . $this->getValue($checkbox) . '"><label for="graduate">Diplomated</label>');  
             }
 
